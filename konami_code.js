b72f208a1c272c7a09f98852c86a0a99857a0d27
Greg Stimson <stimsong@yahoo.com>
2018-01-29 16:38:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-//let index = 0;
+let index = 0;
 
 function init() {
-//  var e = document.querySelector("div.mocha input [e='Konami']");
-let index = 0;
   document.body.addEventListener('keydown', function(e) {
-//    function onKeyDownHandler(e) {
     const key = parseInt(e.which || e.detail);
 
     if (code[index] === key) {
